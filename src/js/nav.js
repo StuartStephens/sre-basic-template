0c6d048a50e5fa3e175f68ba65f49e7d6f774044
Stuart A. Stephens <contact@stuartstephens.com>
2019-03-05 11:02:19 -0500

    adding basic hero with collapsable navbar

--- src/js/nav.js
+++ src/js/nav.js
@@ -2,9 +2,11 @@
 (function($) {
   $(function() { // DOM Ready
 
-  $('.c-mobile-nav__toggle').on('click', function() {
-	  this.classList.toggle('is-active');
-	  $('nav ul').toggle();
+  $('.c-nav-toggle__button').on('click', function() {
+	  // this.classList.toggle('is-active');
+	  $(this).find("span").toggleClass("is-active");
+	  // $(this).find("span:before").attr("content", "\\e800");
+	  $('.c-nav-toggle__overlay').toggle();
 
 	});
 
